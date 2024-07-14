778a3a5ff2700fb5f078575c147a9f2cbf6c5f6c
Sanam Gurung <sanam.bdr.gurung@gmail.com>
2024-07-14 11:37:17 +0545

    init commit

---
+++ android/app/src/main/kotlin/com/mesh/gradient/animated_mesh_gradient/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.mesh.gradient.animated_mesh_gradient
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'pages/transform_viewer_page.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Image Mesh Gradient',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.deepPurple,
+        ),
+        useMaterial3: true,
+      ),
+      home: const TransformViewerPage(),
+    );
+  }
+}
